d157bbae0de9be9321832af618a6ab523ae30228
Erenanhai <namvtp.bi9022@st.usth.edu.vn>
2021-05-13 11:18:31 +0700

    LW 6 update (currently waiting for professor to update the slide)

--- 6.shell.py
+++ 6.shell.py
@@ -1,15 +1,33 @@
+#!/usr/bin/env python3
+
 import subprocess as sp
 
+def pipe(p):
+    scm = p.split("|")
+    seg1 = scm[0].split(" ")
+    seg1.remove("")
+    seg2 = scm[1].split(" ")
+    seg2.remove("")
+    
+    ps = sp.Popen(seg1, stdout=sp.PIPE).communicate()
+    output = sp.Popen(seg2, stdin=ps.stdout)
+    sp.wait()
+
 def command(cm):
-    try:
-        ps = sp.run(cm.split(" "))
-        return ps
-    except Exception:
-        print("Err404: code not found.")
+    #try:
+        if "|" in cm:
+            pipe(cm)
+        else:
+            seg = cm.split(" ")
+            if "" in seg:
+                seg.remove("")
+            sp.run(seg)
+    #except Exception:
+    #    print("Error! Try again.")
 
 while 1:
     ip = input("Shell >> ")
     if ip == "close":
         break
-    else: command(ip)
-    
+    else: 
+        command(ip)
