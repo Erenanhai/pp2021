251a6bfc7c5265ede055a711d1130ff4acf82c2a
Erenanhai <namvtp.bi9022@st.usth.edu.vn>
2021-05-13 09:43:58 +0700

    LW 6 small update (work with basic command without "|")

--- 6.shell.py
+++ 6.shell.py
@@ -1,5 +1,15 @@
 import subprocess as sp
 
+def command(cm):
+    try:
+        ps = sp.run(cm.split(" "))
+        return ps
+    except Exception:
+        print("Err404: code not found.")
+
 while 1:
     ip = input("Shell >> ")
-    ps = sp.run(ip.split(" "))
+    if ip == "close":
+        break
+    else: command(ip)
+    
